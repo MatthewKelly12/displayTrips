682943114d086a286c91cc98213c57eb64b9073b
Matthew Kelly <mkonabike@gmail.com>
2018-04-13 16:54:23 -0500

    initial commit, storage not set up, and needs small styling and function changes

---
+++ storage.js
@@ -0,0 +1,75 @@
+// // Create objects of cities visited
+
+// const portlandOr = {
+//     cityName: "Portland, Oregon",
+//     year: "2017",
+//     pic: "imagesCity/portland.jpg",
+//     attractions: "Japanese Garden, Rose Garden, Ringside steakhouse, The Gorge, Public Transit"
+// };
+
+// const oregonCityOr = {
+//     cityName: "Oregon City, Oregon",
+//     year: "2016",
+//     pic: "imagesCity/portland.jpg",
+//     attractions: "Waterfall, Pig that lives on the corner, My homie Chris, Chris's terrible house rules, Good Beer"
+// };
+
+// const nashville = {
+//     cityName: "Nashville, TN",
+//     year: "2015",
+//     pic:"imagesCity/portland.jpg",
+//     attractions: "Cheekwood, Springwater, Hot Chicken, Humidity, The Piney River"
+// };
+
+// const hiltonHead = {
+//     cityName: "Hilton Head Island, SC",
+//     year: "2014",
+//     pic: "imagesCity/portland.jpg",
+//     attractions: "Ocean, Beer, My Homie John, Skrempfz, Sand in my shoe"
+// };
+
+// const louisville = {
+//     cityName: "Louisville, Ky",
+//     year: "2013",
+//     pic: "imagesCity/portland.jpg",
+//     attractions: "The Cardinals, My Cuz Pat, Cool Bands, Beer, Dogs"
+// };
+
+// const sanDiego = {
+//     cityName: "San Diego, Ca",
+//     year: "2012",
+//     pic: "imagesCity/portland.jpg",
+//     attractions: "Fires, Beach, Good Beer, Sushi, Shady People"
+// };
+// // Create empty object for trips 
+
+// const trips = {};
+
+// //Create empty array myTrips in trips
+
+// trips.myTrips = [];
+
+// // Make function to push city to array
+
+// function makeArray(city) {
+//     trips.myTrips.push(city);
+// }
+
+// // call function w/cities to push to array
+// makeArray(portlandOr);
+// makeArray(oregonCityOr);
+// makeArray(nashville);
+// makeArray(hiltonHead);
+// makeArray(louisville);
+// makeArray(sanDiego);
+
+// // Make sure cities are in object trips
+// console.log(trips.myTrips[0]);
+// console.log(trips.myTrips[1]);
+// console.log(trips.myTrips[2]);
+// console.log(trips.myTrips[3]);
+// console.log(trips.myTrips[4]);
+// console.log(trips.myTrips[5]);
+// console.log(trips.myTrips[5].cityName);
+
+// console.log(nashville.cityName);
