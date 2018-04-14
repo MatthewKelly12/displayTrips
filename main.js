c125d8821547804894718c267e4a87536f0eea92
Matthew Kelly <mkonabike@gmail.com>
2018-04-14 12:23:29 -0500

    storage set up, needs color change

--- main.js
+++ main.js
@@ -1,117 +1,35 @@
-// Create objects of cities visited
-
-const portlandOr = {
-    cityName: "Portland, Oregon",
-    year: "2017",
-    pic: "imagesCity/portland.jpg",
-    attractions: "Japanese Garden, Rose Garden, Ringside steakhouse, The Gorge, Public Transit"
-};
-
-const oregonCityOr = {
-    cityName: "Oregon City, Oregon",
-    year: "2016",
-    pic: "imagesCity/portland.jpg",
-    attractions: "Waterfall, Pig that lives on the corner, My homie Chris, Chris's terrible house rules, Good Beer"
-};
-
-const nashville = {
-    cityName: "Nashville, TN",
-    year: "2015",
-    pic:"imagesCity/portland.jpg",
-    attractions: "Cheekwood, Springwater, Hot Chicken, Humidity, The Piney River"
-};
-
-const hiltonHead = {
-    cityName: "Hilton Head Island, SC",
-    year: "2014",
-    pic: "imagesCity/portland.jpg",
-    attractions: "Ocean, Beer, My Homie John, Skrempfz, Sand in my shoe"
-};
-
-const louisville = {
-    cityName: "Louisville, Ky",
-    year: "2013",
-    pic: "imagesCity/portland.jpg",
-    attractions: "The Cardinals, My Cuz Pat, Cool Bands, Beer, Dogs"
-};
-
-const sanDiego = {
-    cityName: "San Diego, Ca",
-    year: "2012",
-    pic: "imagesCity/portland.jpg",
-    attractions: "Fires, Beach, Good Beer, Sushi, Shady People"
-};
-// Create empty object for trips 
-
-const trips = {};
-
-//Create empty array myTrips in trips
-
-trips.myTrips = [];
-
-// Make function to push city to array
-
-function makeArray(city) {
-    trips.myTrips.push(city);
-}
-
-// call function w/cities to push to array
-makeArray(portlandOr);
-makeArray(oregonCityOr);
-makeArray(nashville);
-makeArray(hiltonHead);
-makeArray(louisville);
-makeArray(sanDiego);
-
-// Make sure cities are in object trips
-console.log(trips.myTrips[0]);
-console.log(trips.myTrips[1]);
-console.log(trips.myTrips[2]);
-console.log(trips.myTrips[3]);
-console.log(trips.myTrips[4]);
-console.log(trips.myTrips[5]);
-console.log(trips.myTrips[5].cityName);
-
-console.log(nashville.cityName);
-
 
 // make function to create div and text content, append to body
-
+let tripsDatabase = JSON.parse(localStorage.getItem("Matt's Trips"));
 let mainBody = document.getElementById("cardTrip");
 function makeDiv () {
-  for (let i = 0; i < trips.myTrips.length; i++) { 
+  for (let i = 0; i < tripsDatabase.myTrips.length; i++) { 
     
     let div1 = document.createElement("div");
      mainBody.appendChild(div1);
 
      let head2 = document.createElement("h2");
-     head2.textContent = trips.myTrips[i].cityName;
+     head2.textContent = tripsDatabase.myTrips[i].cityName;
      div1.appendChild(head2);
 
      let par1 = document.createElement("p");
      let img1 = document.createElement("img");
-     img1.src = trips.myTrips[i].pic;
+     img1.src = tripsDatabase.myTrips[i].pic;
      img1.setAttribute("width", "200");
      img1.setAttribute("height", "200");
      par1.appendChild(img1);
      div1.appendChild(par1);
 
      let par2 = document.createElement("p");
-     par2.textContent = trips.myTrips[i].year;
+     par2.textContent = tripsDatabase.myTrips[i].year;
      div1.appendChild(par2);
 
      let par3 = document.createElement("p");
-     par3.textContent = trips.myTrips[i].attractions;
+     par3.textContent = tripsDatabase.myTrips[i].attractions;
      div1.appendChild(par3);
 
   }
 }
 makeDiv();
-
-
-
-
-
-
 
 
